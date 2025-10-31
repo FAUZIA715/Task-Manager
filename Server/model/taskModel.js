00598ed52323143ff9e68aefc4106150485b9d6d
FAUZIA715 <fauziasimi21@gmail.com>
2025-10-31 10:12:16 +0530

    Added task logic

---
+++ Server/model/taskModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    userId: String,
+    title: String,
+    description: String,
+    completed: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Task", taskSchema);
--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+import taskRoutes from "./router/taskRoutes.js";
 // Load env
 dotenv.config();
 
@@ -15,5 +16,6 @@ app.use(express.json());
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "OK", message: "Server is healthy" });
 });
+app.use("/api/tasks", taskRoutes);
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
